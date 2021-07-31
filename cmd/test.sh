7ce4d12ced7d144732ae0906a85857d38a112d98
zhengzhuang95 <zheng951203@163.com>
2021-07-31 16:35:01 +0800

    优化页面

---
+++ cmd/test.sh
@@ -0,0 +1,13 @@
+###
+ # @Author: zhengzhuang
+ # @Date: 2021-07-31 16:19:22
+ # @LastEditors: zhengzhuang
+ # @LastEditTime: 2021-07-31 16:34:25
+ # @Description: In User Settings Edit
+ # @FilePath: /gin-react-admin/cmd/test.sh
+### 
+
+#!/bin/bash
+cd ../
+echo "运行主程序"
+go run main.go

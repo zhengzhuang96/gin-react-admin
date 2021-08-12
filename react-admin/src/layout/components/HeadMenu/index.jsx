4c56f8c0d4837e7382d6bdb04f5544be071eb4ed
zhengzhuang95 <zheng951203@163.com>
2021-08-12 14:25:16 +0800

    新增全屏状态

--- react-admin/src/layout/components/HeadMenu/index.jsx
+++ react-admin/src/layout/components/HeadMenu/index.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col, Avatar, Dropdown, Menu, Space, Modal } from 'antd';
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
   UserOutlined,
   DownOutlined,
-  ExclamationCircleOutlined
+  ExclamationCircleOutlined,
+  FullscreenOutlined,
+  FullscreenExitOutlined
 } from '@ant-design/icons';
 import styles from './index.module.css';
 import { useHistory } from "react-router-dom";
@@ -14,6 +16,29 @@ const { confirm } = Modal;
 let HeadMenu = (props) => {
   const history = useHistory();
   const [visible, setVisible] = useState(false)
+  const [fullScreenState, setFullScreenState] = useState(false) // 全屏状态
+
+  // 监听全屏状态
+  useEffect(() => {
+    const fullscreenchange = () => {
+      if (fullScreenState) {
+        setFullScreenState(false)
+      } else {
+        setFullScreenState(true)
+      }
+    }
+
+    document.addEventListener("fullscreenchange", fullscreenchange);
+    document.addEventListener("mozfullscreenchange", fullscreenchange);
+    document.addEventListener("webkitfullscreenchange", fullscreenchange);
+    document.addEventListener("msfullscreenchange", fullscreenchange);
+    return () => {
+      document.removeEventListener("fullscreenchange", fullscreenchange);
+      document.removeEventListener("mozfullscreenchange", fullscreenchange);
+      document.removeEventListener("webkitfullscreenchange", fullscreenchange);
+      document.removeEventListener("msfullscreenchange", fullscreenchange);
+    };
+  }, [fullScreenState])
 
   const handleMenuClick = e => {
     if (e === 1) {
@@ -45,6 +70,32 @@ let HeadMenu = (props) => {
     </Menu>
   );
 
+  // 切换全屏效果
+  const setFullScreenMode = () => {
+    const isFullScreen = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen || document.msFullscreenElement
+    if (isFullScreen) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen()
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen()
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen()
+      } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen()
+      }
+    } else {
+      if (document.body.requestFullscreen) {
+        document.body.requestFullscreen()
+      } else if (document.body.mozRequestFullScreen) {
+        document.body.mozRequestFullScreen()
+      } else if (document.body.webkitRequestFullScreen) {
+        document.body.webkitRequestFullScreen()
+      } else if (document.body.msRequestFullscreen) {
+        document.body.msRequestFullscreen()
+      }
+    }
+  }
+
   return (
     <Row className={styles.box}>
       <Col span={6}>
@@ -54,13 +105,17 @@ let HeadMenu = (props) => {
       </Col>
       <Col>
         <Space>
-          <Avatar size={40} icon={<UserOutlined />} />
+          <Avatar size={40} icon={fullScreenState ? <FullscreenExitOutlined /> : <FullscreenOutlined />} style={{ background: "none" }} onClick={setFullScreenMode} />
           <Dropdown
             overlay={menu}
             onVisibleChange={() => handleVisibleChange()}
-            visible={visible}
+            placement="topRight"
+            arrow
           >
-            <a className="ant-dropdown-link" href="onclick:" onClick={e => e.preventDefault()} rel="noopener noreferrer">设置 <DownOutlined /></a>
+            <Space>
+              <Avatar size={40} icon={<UserOutlined />} />
+              <a className="ant-dropdown-link" href="onclick:" onClick={e => e.preventDefault()} rel="noopener noreferrer"><DownOutlined /></a>
+            </Space>
           </Dropdown>
         </Space>
       </Col>

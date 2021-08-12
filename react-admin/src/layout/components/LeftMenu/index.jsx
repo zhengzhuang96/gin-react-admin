6285d98785f327a8369227abd51f738e87ebb1ed
zhengzhuang95 <zheng951203@163.com>
2021-08-12 10:07:51 +0800

    优化web

--- react-admin/src/layout/components/LeftMenu/index.jsx
+++ react-admin/src/layout/components/LeftMenu/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
 import { Menu } from 'antd';
 import {
@@ -8,20 +8,12 @@ import {
 import indexRoutes from "../../../routes/indexRoutes";
 import index from "./index.module.css"
 const { SubMenu } = Menu;
-console.log('123123123')
 function LeftMenu(props) {
   let history = useHistory();
-  const [openKey, setopenKey] = useState(['/index'])
 
   const handleClick = to => {
     history.push(to);
   }
-
-  useEffect(() => {
-    console.log(history.location.pathname)
-    setopenKey([history.location.pathname])
-  }, [history])
-
 
   return (
     <div className={index.leftNav}>
@@ -29,8 +21,8 @@ function LeftMenu(props) {
         <h3>{props.collapsedState ? "GIN" : "GIN-REACT-ADMIN"}</h3>
       </div>
       <Menu
-        defaultSelectedKeys={openKey}
-        defaultOpenKeys={openKey}
+        defaultSelectedKeys={[history.location.pathname]}
+        defaultOpenKeys={[history.location.pathname]}
         mode="inline"
         theme="dark"
       >
@@ -41,12 +33,12 @@ function LeftMenu(props) {
                 {
                   path.children.map((paths, indexs) => {
                     return (
-                      <Menu.Item key={indexs} onClick={() => handleClick(paths.path)}>{paths.meta.title}</Menu.Item>
+                      <Menu.Item key={paths.path} onClick={() => handleClick(paths.path)}>{paths.meta.title}</Menu.Item>
                     )
                   })
                 }
               </SubMenu>
-              : <Menu.Item key={index} onClick={() => handleClick(path.path)} icon={<ContainerOutlined />}>
+              : <Menu.Item key={path.path} onClick={() => handleClick(path.path)} icon={<ContainerOutlined />}>
                 {path.meta.title}
               </Menu.Item>
           )
